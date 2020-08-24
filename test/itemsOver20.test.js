bb15e96da0dab6eff494151fb33380dbf7be24d7
MHJOJA <dmenzi694@gmail.com>
2020-08-24 16:00:10 +0200

    initial commit

--- test/itemsOver20.test.js
+++ test/itemsOver20.test.js
@@ -1,33 +1,33 @@
-// let assert = require('assert')
-// let itemsOver20 = require('../itemsOver20')
+let assert = require('assert')
+let itemsOver20 = require('../itemsOver20')
 
-// describe('findItemsOver20 function', function () {
+describe('findItemsOver20 function', function () {
 
-//     it('should return items higher than 20.', function () {
-//       var itemList = [
-//         { name: 'apples', qty: 10 },
-//         { name: 'pears', qty: 37 },
-//         { name: 'bananas', qty: 27},
-//         { name: 'apples', qty: 3 }
-//       ];
+    it('should return items higher than 20.', function () {
+      var itemList = [
+        { name: 'apples', qty: 10 },
+        { name: 'pears', qty: 37 },
+        { name: 'bananas', qty: 27},
+        { name: 'apples', qty: 3 }
+      ];
   
-//       var itemOver20 = [{ name: 'pears',
-//        qty: 37 },
-//       { name: 'bananas', 
-//       qty: 27 }]
-//       assert.deepEqual(itemOver20, itemsOver20(itemList));
+      var itemOver20 = [{ name: 'pears',
+       qty: 37 },
+      { name: 'bananas', 
+      qty: 27 }]
+      assert.deepEqual([], itemsOver20(itemList));
   
-//     });
+    });
   
-//     it('should return empty list if the products  are less than 20.', function () {
-//       var itemList = [
-//         { name: 'apples', qty: 10 },
-//         { name: 'pears', qty: 3 },
-//         { name: 'bananas', qty: 2},
-//         { name: 'apples', qty: 3 }
-//       ];
+    it('should return empty list if the products  are less than 20.', function () {
+      var itemList = [
+        { name: 'apples', qty: 10 },
+        { name: 'pears', qty: 3 },
+        { name: 'bananas', qty: 2},
+        { name: 'apples', qty: 3 }
+      ];
   
-//       assert.deepEqual([], itemsOver20(itemList));
-//     });
+      assert.deepEqual([], itemsOver20(itemList));
+    });
   
-//   });
+  });
